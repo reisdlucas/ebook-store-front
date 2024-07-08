b7f1ffae659ba20d465555c43be3eedb8b499b98
reisdlucas <reisdlucas@icloud.com>
2024-07-08 14:15:09 -0300

    add card products

--- src/app/feature/list/list.component.ts
+++ src/app/feature/list/list.component.ts
@@ -2,11 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatCardModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
