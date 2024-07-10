b1b3c5cdae7605a3daefa27fc4dc5ed14b95b5e5
Lucas Reis <reisdlucas@icloud.com>
2024-07-10 13:40:25 -0300

    refactor import from MatBottonModule

--- src/app/features/list/components/card/card.component.ts
+++ src/app/features/list/components/card/card.component.ts
@@ -1,11 +1,12 @@
 import { Component, computed, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { Product } from '../../../../shared/interfaces/product.interface';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [MatCardModule, MatCardModule],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
