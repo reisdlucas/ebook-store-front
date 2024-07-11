af7ee943919ce2d85fc162d66e0b193b0be1f8e8
reisdlucas <reisdlucas@icloud.com>
2024-07-11 09:47:08 -0300

    refactor create product path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,6 @@ export const routes: Routes = [{
 },
 {
     path: 'create-product',
-    component: CreateComponent,
     loadComponent: () =>
         import('./features/create/create.component').then(
             (m) => m.CreateComponent
