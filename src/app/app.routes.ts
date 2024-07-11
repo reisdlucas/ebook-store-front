8242189056d9f89513939086eb216b11739e03c0
reisdlucas <reisdlucas@icloud.com>
2024-07-11 09:29:51 -0300

    add router to create product

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './feature/list/list.component'
+import { CreateComponent } from './features/create/create.component';
 
 export const routes: Routes = [{
     path: '',
     component: ListComponent
-}];
+},
+{
+    path: 'create-product',
+    component: CreateComponent,
+},
+];
