46f5e9461907704d83d80c4adc8ce414fceb0176
reisdlucas <reisdlucas@icloud.com>
2024-07-15 15:21:33 -0300

    #16  salve function

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { ProductsService } from '../../shared/services/products.service';
 
 @Component({
   selector: 'app-create',
@@ -12,15 +13,22 @@ import { MatInputModule } from '@angular/material/input';
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
+
+  productsService = inject(ProductsService);
+
   form = new FormGroup({
-    title: new FormControl<String>('', {
+    title: new FormControl<string>('', {
       nonNullable: true,
       validators: Validators.required,
     }),
   });
 
   onSubmit() {
-    this.form.controls.title.value;
+    this.productsService.post({
+      title: this.form.controls.title.value
+    })
+      .subscribe(() => {
+        alert('Sucesso!');
+      });
   }
-
 }
